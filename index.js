40e263ac677fea6e899f9d33082a5a0b442e1b07
glebselukov <prospik@ya.ru>
2017-08-24 10:38:26 +0700

    Init [v1]

---
+++ index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { AppRegistry, NativeModules, View } from 'react-native';
+import TextField from './lib/TextField';
+
+const { backSpace, insertText } = NativeModules.RNAllmaxKeyboard;
+
+const keyboardTypeRegistry = {};
+
+function registerKeyboard(keyboardType, keyboardProvider) {
+  keyboardTypeRegistry[keyboardType] = keyboardProvider;
+}
+
+class CustomKeyboardContainer extends Component {
+  render() {
+    const {tag, keyboardType} = this.props;
+    const factory = keyboardTypeRegistry[keyboardType];
+    if (!factory) {
+      console.warn(`Custom keyboard type ${keyboardType} not registered.`);
+      return null;
+    }
+    const Keyboard = factory();
+    return <Keyboard tag={tag} />;
+  }
+}
+
+AppRegistry.registerComponent("AllMaxKeyboard", () => CustomKeyboardContainer);
+
+export {
+  registerKeyboard,
+  TextField,
+  backSpace,
+  insertText,
+}
